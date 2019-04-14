537c7ee9cb274a904ec7dba77058f91530fb818d
Ronald Moesbergen <github@rmoesbergen.nl>
2019-04-14 13:34:42 +0200

    Fix auto-add, make members visiable in admin site

--- django/corvee/corvee/src/models.py
+++ django/corvee/corvee/src/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Persoon(models.Model):
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name_plural = "Personen"
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
@@ -11,5 +16,5 @@ class Persoon(models.Model):
     picture = models.TextField(blank=True, null=True, verbose_name='Foto', editable=True)
     day_friday = models.BooleanField(null=False, default=False)
     day_saturday = models.BooleanField(null=False, default=False)
-    latest = models.DateTimeField(auto_now_add=True, blank=False)
+    latest = models.DateTimeField(default=now, blank=False, editable=True)
     absent = models.DateField(blank=True, null=True, default=None)
