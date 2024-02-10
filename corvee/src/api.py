992720a34478ac53916664dcfc55a414384688a0
ILoveAndLikePizza <nathankoning@gmail.com>
2024-02-10 12:41:48 +0100

    Require a Bearer token for /api/v1/selected

--- corvee/src/api.py
+++ corvee/src/api.py
@@ -1,13 +1,13 @@
 from django.http.response import JsonResponse
-from django.views import View
 
 from corvee.src.models import Persoon
+from corvee.src.mixins import TokenRequiredMixin
 
 
-class SelectedV1(View):
+class SelectedV1(TokenRequiredMixin):
     def get(self, request, *args, **kwargs):
         selected = Persoon.objects.filter(selected=True)
         present = Persoon.objects.filter(absent=False)
         names = [person.short_name for person in selected]
         present_names = [person.short_name for person in present]
-        return JsonResponse({"selected": names, "present": present_names})
+        return JsonResponse({"selected": names, "present": present_names, })
